4d3ed2593f9d4abad1646474f42beec3d94c8d57
Jean Pierre Niyitanga <jpniyitanga@gmail.com>
2023-06-30 17:57:08 -0400

    read and update store attributes

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,8 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+@store1.update(name: "Ottawa")
+@store1.save
+pp @store1
