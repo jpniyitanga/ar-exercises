37e16d1f2016fec3b742406705e2ae6d42902ae8
Jean Pierre Niyitanga <jpniyitanga@gmail.com>
2023-06-30 20:12:52 -0400

    Calculations

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts "-----TOTAL ANNUAL REVENUE-----"
+puts Store.sum("annual_revenue")
+
+puts "-----AVERAGE ANNUAL REVENUE-----"
+puts Store.average("annual_revenue")
+
+puts "-----NUMBER OF STORES GENERATING MORE THAN $1M-----"
+pp Store.where("annual_revenue >= ?", 1000000).count
