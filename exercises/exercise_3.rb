18adf869f5124647e53102cd0766b43ff9fd5825
Jean Pierre Niyitanga <jpniyitanga@gmail.com>
2023-06-30 20:45:46 -0400

    Define association between store and employee classes + seed employees table

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -7,6 +7,11 @@ puts "----------"
 
 # Your code goes here ...
 @store3 = Store.find_by(id: 3)
+
 @store3.destroy
 
+#Need to re-run exercise_1 to recreate the Gastown store deleted above
+
 puts "There are #{Store.count} stores in the database."
+
+
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,13 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 50)
+@store1.employees.create(first_name: "Alice", last_name: "Smith", hourly_rate: 55)
+@store2.employees.create(first_name: "Alpha", last_name: "Romeo", hourly_rate: 35)
+@store2.employees.create(first_name: "Mary", last_name: "Roman", hourly_rate: 70)
+@store2.employees.create(first_name: "Susan", last_name: "Nemis", hourly_rate: 40.55)
+@store3.employees.create(first_name: "Allan", last_name: "Koring", hourly_rate: 60)
+@store3.employees.create(first_name: "Andrea", last_name: "Aderemi", hourly_rate: 51)
+@store3.employees.create(first_name: "Allison", last_name: "Thompson", hourly_rate: 46)
+
